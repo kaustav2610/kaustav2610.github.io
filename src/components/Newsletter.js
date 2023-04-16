d12d36d1f821783d019493bdf867805013cd9e89
kaustav2610 <kastav2610@gmail.com>
2023-04-16 15:41:48 +0530

    first commit

---
+++ src/components/Newsletter.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
+
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    if (status === 'success') clearFields();
+  }, [status])
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   email &&
+  //   email.indexOf("@") > -1 &&
+  //   onValidated({
+  //     EMAIL: email
+  //   })
+  // }
+
+  const clearFields = () => {
+    setEmail('');
+  }
+
+  return (
+      <Col lg={12}>
+        <div className="newsletter-bx wow slideInUp">
+          <Row>
+            <Col lg={12} md={6} xl={5}>
+              <h3>Connect With Me</h3>
+              {status === 'sending' && <Alert>Sending...</Alert>}
+              {status === 'error' && <Alert variant="danger">{message}</Alert>}
+              {status === 'success' && <Alert variant="success">{message}</Alert>}
+            </Col>
+            <Col md={6} xl={7}>
+              <form action="https://formspree.io/f/moqzpbrd" method="POST" >
+                <div className="new-email-bx">
+                  <input name="Email" value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
+            </Col>
+          </Row>
+        </div>
+      </Col>
+  )
+}
